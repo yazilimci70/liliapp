840c664db167cfeab0cc5baacf00f31ed004303e
yazilimci70 <141878774+yazilimci70@users.noreply.github.com>
2023-08-10 02:12:56 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const inputField = document.getElementById('inputField');
-
-inputField.addEventListener('input', function() {
-  if (this.value !== '') {
-    this.classList.add('has-value');
-  } else {
-    this.classList.remove('has-value');
-  }
-});
